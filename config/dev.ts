cd4b36f73524eccafc4f870001740ed93ccca3c0
Sourabh Yogi <sorabh.yogi06@gmail.com>
2025-07-28 22:48:45 +0530

    added Iam role stack

--- config/dev.ts
+++ config/dev.ts
@@ -35,17 +35,17 @@ export const devConfig = {
         },
       ],
     },
-    {
-        name: `${G.projectName}-${envName}-test`,       // Use envName string here
-        assumeService: 'ec2.amazonaws.com',
-        policies: [
-          {
-            name: `${G.projectName}-${envName}-policy-dev`,  // add suffix for clarity
-            actions: ['*'],
-            resources: ['*'],
-          },
-        ],
-      },
+    // {
+    //     name: `${G.projectName}-${envName}-test`,       // Use envName string here
+    //     assumeService: 'ec2.amazonaws.com',
+    //     policies: [
+    //       {
+    //         name: `${G.projectName}-${envName}-policy-dev`,  // add suffix for clarity
+    //         actions: ['*'],
+    //         resources: ['*'],
+    //       },
+    //     ],
+    //   },
   ],
 
   // You can add other stack configs below
